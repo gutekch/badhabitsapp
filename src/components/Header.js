e8a1083c3167329eb71c555c54099622b22442c3
gutekch <gutekch@gmail.com>
2024-05-23 13:16:11 +0800

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../styles.css';
+
+export const Header = () => {
+  return (
+    <div>
+        <h1 className='main-header'>BAD HABITS</h1>
+        <h3 className='header-describtion'>BE IN CONTROL OF YOUR LIFE AGAIN! FIGHT THE HABBITS THAT ARE BAD FOR YOU!</h3>
+    </div>
+  )
+}
